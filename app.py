079dda411828bb2e851e84bdcaf8003febba37fe
SarahCarneir0 <109230924+SarahCarneir0@users.noreply.github.com>
2022-12-09 11:06:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -49,11 +49,12 @@ if data is not None:
         col1, col2, col3 = st.columns(3)
 
         with col1:
-
+            if 'repkata1.keys' in df:
             s2_r1_trials = df['repkata1.keys'].loc[df['repkata1.keys'].isin(['right', 'left']) ].count()
             s2_r1_answers = df.loc[df['repkata1.keys'].isin(['right', 'left']), ['repkata1.keys', 'repkata1.corr']]
             s2_r1_errors = s2_r1_answers[s2_r1_answers['repkata1.corr'] == 0].value_counts()
-            
+           
+            elif 'reppata1.keys' in df:
             s2_r1_trials = df['reppata1.keys'].loc[df['reppata1.keys'].isin(['right', 'left']) ].count()
             s2_r1_answers = df.loc[df['reppata1.keys'].isin(['right', 'left']), ['reppata1.keys', 'reppata1.corr']]
             s2_r1_errors = s2_r1_answers[s2_r1_answers['reppata1.corr'] == 0].value_counts()
@@ -112,10 +113,12 @@ if data is not None:
         subtitle_3 = '<p style="font-family:Courier; color:Black; font-size: 40px; font-weight:bold;">Stage 3 = Noise</p>'
         st.markdown(subtitle_3, unsafe_allow_html=True)
         
+        if 'repkata2.keys' in df:
         s3_r1_trials = df['repkata2.keys'].loc[df['repkata2.keys'].isin(['right', 'left']) ].count()
         s3_r1_answers = df.loc[df['repkata2.keys'].isin(['right', 'left']), ['repkata2.keys', 'repkata2.corr']]
         s3_r1_errors = s3_r1_answers[s3_r1_answers['repkata2.corr'] == 0].value_counts()
         
+        if 'reppata2.keys' in df:
         s3_r1_trials = df['reppata2.keys'].loc[df['reppata2.keys'].isin(['right', 'left']) ].count()
         s3_r1_answers = df.loc[df['reppata2.keys'].isin(['right', 'left']), ['reppata2.keys', 'reppata2.corr']]
         s3_r1_errors = s3_r1_answers[s3_r1_answers['reppata2.corr'] == 0].value_counts()
